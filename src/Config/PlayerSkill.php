204de88c9e90cf08cdfe479caa2f5a82e8794fe1
Kevin DEFARGE <1819360+kdefarge@users.noreply.github.com>
2024-04-22 19:53:29 +0200

    Add : - create a enum for PlayerGender - create a enum for PlayerSkill - Make fixture for gender and skill based on own enum - Make fixture for players - Make fixture for encounters without result score yet

---
+++ src/Config/PlayerSkill.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Config;
+
+enum PlayerSkill: string
+{
+    case Low = 'low';
+    case Middle = 'middle';
+    case High = 'high';
+}
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,80 @@
 
 namespace App\DataFixtures;
 
+use App\Config\PlayerGender;
+use App\Config\PlayerSkill;
+use App\Entity\Encounter;
+use App\Entity\Gender;
+use App\Entity\Player;
+use App\Entity\Skill;
+use DateTimeImmutable;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory as Faker;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Faker::create('fr_FR');
+
+        // Create genders based on PlayerGender
+        $genders = [];
+
+        foreach (PlayerGender::cases() as $playerGender) {
+            $gender = new Gender();
+            $gender->setName($playerGender->value);
+            $manager->persist($gender);
+            $genders[$playerGender->value] = $gender;
+        }
+
+        // Create genders based on Playerskill
+        $skills = [];
+
+        foreach (PlayerSkill::cases() as $playerSkill) {
+            $skill = new Skill();
+            $skill->setName($playerSkill->value);
+            $manager->persist($skill);
+            $skills[$playerSkill->value] = $skill;
+        }
+
+        // Create of 20 Players
+        $players = [];
+
+        for ($i = 0; $i < 20; $i++) {
+
+            /** @var Gender $gender */
+            $gender = $faker->randomElement($genders);
+
+            /** @var Skill $skill */
+            $skill = $faker->randomElement($skills);
+
+            $player = new Player();
+            $player->setFirstname($faker->firstName($gender->getName()));
+            $player->setLastname($faker->lastName($gender->getName()));
+            $player->setGender($gender);
+            $player->setSkill($skill);
+            $manager->persist($player);
+            $players[] = $player;
+        }
+
+        // Create of 60 encounters
+
+        for ($i = 0; $i < 60; $i++) {
+
+            $encounter = new Encounter();
+            $encounter->setFinished($faker->boolean(80));
+            $createAt = $faker->dateTimeInInterval("-200 days", "-50 days", "Europe/Paris");
+            $encounter->setCreatedAt(DateTimeImmutable::createFromMutable($createAt));
+            if ($encounter->isFinished()) {
+                $encounter->setTeam1Won($faker->boolean());
+                $encounter->setUpdatedAt(DateTimeImmutable::createFromMutable($faker->dateTimeInInterval($createAt, "-1 days", "Europe/Paris")));
+            }
+            $manager->persist($encounter);
+
+            /** @var Player $gender */
+            $encounterPlayers = $faker->randomElements($players, 4);
+        }
 
         $manager->flush();
     }
