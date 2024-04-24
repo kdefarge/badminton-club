b6d35ad49dff348640a13fd88e3c2d3209b75296
Kevin DEFARGE <1819360+kdefarge@users.noreply.github.com>
2024-04-24 21:44:19 +0200

    Add: - Make CRUD Player - Add homepage with menu to access CRUD player

---
+++ src/Form/PlayerType.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Gender;
+use App\Entity\Player;
+use App\Entity\Skill;
+use App\Entity\Tournament;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PlayerType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('firstname')
+            ->add('lastname')
+            ->add('tournaments', EntityType::class, [
+                'class' => Tournament::class,
+                'choice_label' => 'id',
+                'multiple' => true,
+            ])
+            ->add('gender', EntityType::class, [
+                'class' => Gender::class,
+                'choice_label' => 'id',
+            ])
+            ->add('skill', EntityType::class, [
+                'class' => Skill::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Player::class,
+        ]);
+    }
+}
