0fb1a5a7e66654fbc26fc5f0ec6868323c832cc8
Kevin DEFARGE <1819360+kdefarge@users.noreply.github.com>
2024-04-24 16:27:08 +0200

    Add : - create a TournamentManager service not implemented yet

---
+++ src/Service/TournamentManager.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\Encounter;
+use App\Entity\Tournament;
+
+class TournamentManager
+{
+    public function getRandomEncounters(Tournament $tournament): ?Encounter
+    {
+        return null;
+    }
+}
