9732e788e677ede976b0049783141eea2fd2757f
Kevin DEFARGE <1819360+kdefarge@users.noreply.github.com>
2024-04-25 20:32:16 +0200

    add - make:crud Encounter

---
+++ src/Controller/EncounterController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Encounter;
+use App\Form\EncounterType;
+use App\Repository\EncounterRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/encounter')]
+class EncounterController extends AbstractController
+{
+    #[Route('/', name: 'app_encounter_index', methods: ['GET'])]
+    public function index(EncounterRepository $encounterRepository): Response
+    {
+        return $this->render('encounter/index.html.twig', [
+            'encounters' => $encounterRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_encounter_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $encounter = new Encounter();
+        $form = $this->createForm(EncounterType::class, $encounter);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($encounter);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_encounter_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('encounter/new.html.twig', [
+            'encounter' => $encounter,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_encounter_show', methods: ['GET'])]
+    public function show(Encounter $encounter): Response
+    {
+        return $this->render('encounter/show.html.twig', [
+            'encounter' => $encounter,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_encounter_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Encounter $encounter, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(EncounterType::class, $encounter);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_encounter_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('encounter/edit.html.twig', [
+            'encounter' => $encounter,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_encounter_delete', methods: ['POST'])]
+    public function delete(Request $request, Encounter $encounter, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$encounter->getId(), $request->getPayload()->get('_token'))) {
+            $entityManager->remove($encounter);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_encounter_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Form/EncounterType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Encounter;
+use App\Entity\Tournament;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EncounterType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('isFinished')
+            ->add('isTeam1Won')
+            ->add('createdAt', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('updatedAt', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('tournament', EntityType::class, [
+                'class' => Tournament::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Encounter::class,
+        ]);
+    }
+}
