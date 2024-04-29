2d25f70c95314bbb9dbebd1d8510a29094fcdef9
Kevin DEFARGE <1819360+kdefarge@users.noreply.github.com>
2024-04-30 00:32:34 +0200

    Add tournament controller, make tournament index template and add a tournament link to main menu

---
+++ src/Controller/TournamentController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\TournamentRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/tournament')]
+class TournamentController extends AbstractController
+{
+    #[Route('/', name: 'app_tournament_index', methods: ['GET'])]
+    public function index(TournamentRepository $tournamentRepository): Response
+    {
+        return $this->render('tournament/index.html.twig', [
+            'tournaments' => $tournamentRepository->findAll(),
+        ]);
+    }
+}
