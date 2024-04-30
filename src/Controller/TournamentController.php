94cfe4332727e9bbdad7dcb70f3cf5482543a7ff
Kevin DEFARGE <1819360+kdefarge@users.noreply.github.com>
2024-04-30 03:18:27 +0200

    Add create new tournament

--- src/Controller/TournamentController.php
+++ src/Controller/TournamentController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Tournament;
+use App\Form\TournamentType;
 use App\Repository\TournamentRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -15,6 +19,28 @@ class TournamentController extends AbstractController
     {
         return $this->render('tournament/index.html.twig', [
             'tournaments' => $tournamentRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_tournament_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $tournament = new Tournament();
+        $form = $this->createForm(TournamentType::class, $tournament);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $tournament->setCreatedAt(date_create_immutable());
+            $entityManager->persist($tournament);
+            
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_tournament_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('tournament/new.html.twig', [
+            'form' => $form,
         ]);
     }
 }
